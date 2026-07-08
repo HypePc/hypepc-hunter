e00bcd0276c071d6cffa388b1a6fe79f90a18630
HypePc <respectionale30@gmail.com>
2026-07-08 02:05:45 +0200

    Create languages.py

---
+++ bot/languages.py
@@ -0,0 +1,16 @@
+LANGUAGES = {
+    "ru": {
+        "welcome": "🔥 Добро пожаловать в HypePC Hunter!",
+        "choose": "Выберите действие:"
+    },
+
+    "pl": {
+        "welcome": "🔥 Witamy w HypePC Hunter!",
+        "choose": "Wybierz opcję:"
+    },
+
+    "en": {
+        "welcome": "🔥 Welcome to HypePC Hunter!",
+        "choose": "Choose an option:"
+    }
+}
